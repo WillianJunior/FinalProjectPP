a43c6d9e57bf93ce9a4a79d07b0effff64a70c71
Willian Junior <guns945@gmail.com>
2016-09-21 19:29:37 -0300

    Fixed algorithm, however, it can only work with positive edges.

--- serial.cpp
+++ serial.cpp
@@ -35,6 +35,30 @@ public:
 void relax(const vertex_t &v, const weight_t &x, const delta_t& delta, 
 	map<vertex_t, weight_t> &tent, map<index_t, list<vertex_t>> &B);
 
+void printB(map<index_t, list<vertex_t>> B) {
+	for (pair<index_t, list<vertex_t>> p : B) {
+		cout << "index = " << p.first << ":" << endl;
+		for (vertex_t v : p.second) {
+			cout << "\t" << v << endl;
+		}
+	}
+}
+
+void printHL(map<vertex_t, list<edge_t>> hl) {
+	for (pair<index_t, list<edge_t>> p : hl) {
+		cout << "index = " << p.first << ":" << endl;
+		for (edge_t e : p.second) {
+			cout << "\t" << e.from << ":" << e.to << " = " << e.weight << endl;
+		}
+	}	
+}
+
+void printTent(map<vertex_t, weight_t> tent) {
+	for (pair<vertex_t, weight_t> p : tent) {
+		cout << "0:" << p.first << " = " << p.second << endl;
+	}
+}
+
 string usage = "usage: ./serial.out [input_graph] [delta]";
 
 int main (int argc, char** argv) {
@@ -88,27 +112,33 @@ int main (int argc, char** argv) {
 			light[e.from].emplace_back(e);
 	}
 
+	// cout << "heavy:" << endl;
+	// printHL(heavy);
+	// cout << endl << "light:" << endl;
+	// printHL(light);
+
+	vertex_t s = 0; // TODO: set start
+
 	// gen tent
 	map<vertex_t, weight_t> tent;
-	for (edge_t e : E)
-		tent[e.from] = FLT_MAX;
+	for (edge_t e : E) {
+		tent[e.to] = FLT_MAX;
+	}
+	tent[s] = FLT_MAX;
 
 	// gen B
 	map<index_t, list<vertex_t>> B;
 
-	vertex_t s = 0; // TODO: set start
 	relax(s, 0, delta, tent, B);
 	index_t i = 0;
 
 	while (B.size() != 0) {
+		// cout << "S = 0" << endl;
 		list<vertex_t> S;
-		cout << "S = 0" << endl;
 
-		// while B[i] != {}
-		cout << "while B[i] != {}" << endl;
+		// cout << "while B[i] != {}" << endl;
 		while (B[i].size() != 0) {
-			// Req = {(w; tent(v) + c(v,w)) | v in B[i] and (v,w) in light(v)}
-			cout << "Req = {(w; tent(v) + c(v,w)) | v in B[i] and (v,w) in light(v)}" << endl;
+			// cout << "Req = {(w; tent(v) + c(v,w)) | v in B[i] and (v,w) in light(v)}" << endl;
 			list<pair<vertex_t, weight_t>> Req;
 			for (vertex_t v : B[i]) {
 				for (edge_t e1 : E) {
@@ -121,23 +151,19 @@ int main (int argc, char** argv) {
 				}
 			}
 
-			// S = S ++ B[i]
-			// B[i] = {}
-			cout << "S = S ++ B[i]; B[i] = {}" << endl;
-			for (vertex_t v : B[i]) {
-				S.emplace_back(v);
-				B[i].remove(v);
+			// cout << "S = S ++ B[i]; B[i] = {}" << endl;
+			for (list<vertex_t>::const_iterator v=B[i].cbegin(); v!=B[i].cend();) {
+				S.emplace_back(*v);
+				v = B[i].erase(v);
 			}
 
-			// for each (v,x) in Req do relax(v,x)
-			cout << "for each (v,x) in Req do relax(v,x)" << endl;
+			// cout << "for each (v,x) in Req do relax(v,x)" << endl;
 			for (pair<vertex_t, weight_t> p : Req) {
 				relax(p.first, p.second, delta, tent, B);
 			}
 		}
 
-		// Req = {(w; tent(v) + c(v,w)) | v in S and (v,w) in heavy(v)}
-		cout << "Req = {(w; tent(v) + c(v,w)) | v in S and (v,w) in heavy(v)}" << endl;
+		// cout << "Req = {(w; tent(v) + c(v,w)) | v in S and (v,w) in heavy(v)}" << endl;
 		list<pair<vertex_t, weight_t>> Req;
 		for (vertex_t v : S) {
 			for (edge_t e1 : E) {
@@ -150,25 +176,25 @@ int main (int argc, char** argv) {
 			}
 		}
 
-		// for each (v,x) in Req do relax(v,x)
-		cout << "for each (v,x) in Req do relax(v,x)" << endl;
+		// cout << "for each (v,x) in Req do relax(v,x)" << endl;
 		for (pair<vertex_t, weight_t> p : Req) {
 			relax(p.first, p.second, delta, tent, B);
 		}
 
+		B.erase(i);
 		i++;
+		// printTent(tent);
 	}
 
-	for (pair<vertex_t, weight_t> p : tent) {
-		cout << "0:" << p.first << " = " << p.second << endl;
-	}
+	printTent(tent);
 }
 
 void relax(const vertex_t &v, const weight_t &x, const delta_t& delta, 
 	map<vertex_t, weight_t> &tent, map<index_t, list<vertex_t>> &B) {
 
 	if (x < tent[v]) {
-		B[floor(tent[v]/delta)].remove(v);
+		if (B.find(floor(tent[v]/delta)) != B.end())
+			B[floor(tent[v]/delta)].remove(v);
 		B[floor(x/delta)].emplace_back(v);
 		tent[v] = x;
 	}
