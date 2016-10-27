0c57e38a5bbdcd8fca14f87292f040c4b565ee3e
Willian Junior <guns945@gmail.com>
2016-10-27 13:05:41 -0200

    heavy in array format

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ all:
 	@g++ -std=c++11 -ggdb -gdwarf-2 serial.cpp -o serial.out -lm
 
 test: all
-	@./serial.out in.txt 0 < in.txt > t
+	@./serial.out in.txt 0 5 < in.txt > t
 	@diff t out.txt
 	@-rm t
--- serial.cpp
+++ serial.cpp
@@ -32,6 +32,12 @@ public:
 	}
 };
 
+// dynamic sized array in a struct
+typedef struct {
+	int last=-1;
+	edge_t edges[];
+} edge_s;
+
 void relax(const vertex_t &v, const weight_t &x, const delta_t& delta, 
 	map<vertex_t, weight_t> &tent, map<index_t, list<vertex_t>> &B);
 
@@ -59,18 +65,21 @@ void printTent(map<vertex_t, weight_t> tent) {
 	}
 }
 
-string usage = "usage: ./serial.out [input_graph] [delta]";
+string usage = "usage: ./serial.out <input_graph> <delta> <number of vertices>";
 
 int main (int argc, char** argv) {
 	
 	// get args
-	if (argc != 3) {
+	if (argc != 4) {
 		cout << usage << endl;
 		return 0;
 	}
 
 	// get delta
 	delta_t delta = stof(argv[2]) == 0 ? 0.0001 : stof(argv[2]);
+
+	// get number of vertices
+	int n_vertices = stoi(argv[3]);
 
 	// get graph
 	ifstream adj_list_file;
@@ -103,11 +112,17 @@ int main (int argc, char** argv) {
 	// }
 
 	// gen heavy and light
-	map<vertex_t, list<edge_t>> heavy;
+	map<vertex_t, list<edge_t>> heavy_m;
 	map<vertex_t, list<edge_t>> light;
+	
+	int edges_count[n_vertices];
+	for (int i=0; i<n_vertices; i++)
+		edges_count[i] = 0;
+
 	for (edge_t e : E) {
+		edges_count[e.from]++;
 		if (e.weight > delta)
-			heavy[e.from].emplace_back(e);
+			heavy_m[e.from].emplace_back(e);
 		else
 			light[e.from].emplace_back(e);
 	}
@@ -132,13 +147,33 @@ int main (int argc, char** argv) {
 	relax(s, 0, delta, tent, B);
 	index_t i = 0;
 
+	// get max number of edges of all nodes
+	int max_edges = 0;
+	for (int i=0; i<n_vertices; i++)
+		if (edges_count[i] > max_edges)
+			max_edges = edges_count[i];
+
+	// convert heavy and light to arrays
+	edge_s* heavy = (edge_s*)malloc(n_vertices * (sizeof(edge_s) + max_edges));
+	for (int i=0; i<n_vertices; i++) {
+		heavy[i].last = -1;
+	}
+
+	for (pair<vertex_t, list<edge_t>> p : heavy_m) {
+		for (edge_t e : p.second) {
+			heavy[p.first].last++;
+			heavy[p.first].edges[heavy[p.first].last] = e;
+		}
+	}
+
+
 	while (B.size() != 0) {
-		// cout << "S = 0" << endl;
+		cout << "S = 0" << endl;
 		list<vertex_t> S;
 
-		// cout << "while B[i] != {}" << endl;
+		cout << "while B[i] != {}" << endl;
 		while (B[i].size() != 0) {
-			// cout << "Req = {(w; tent(v) + c(v,w)) | v in B[i] and (v,w) in light(v)}" << endl;
+			cout << "Req = {(w; tent(v) + c(v,w)) | v in B[i] and (v,w) in light(v)}" << endl;
 			list<pair<vertex_t, weight_t>> Req;
 			for (vertex_t v : B[i]) {
 				for (edge_t e1 : E) {
@@ -151,39 +186,39 @@ int main (int argc, char** argv) {
 				}
 			}
 
-			// cout << "S = S ++ B[i]; B[i] = {}" << endl;
+			cout << "S = S ++ B[i]; B[i] = {}" << endl;
 			for (list<vertex_t>::const_iterator v=B[i].cbegin(); v!=B[i].cend();) {
 				S.emplace_back(*v);
 				v = B[i].erase(v);
 			}
 
-			// cout << "for each (v,x) in Req do relax(v,x)" << endl;
+			cout << "for each (v,x) in Req do relax(v,x)" << endl;
 			for (pair<vertex_t, weight_t> p : Req) {
 				relax(p.first, p.second, delta, tent, B);
 			}
 		}
 
-		// cout << "Req = {(w; tent(v) + c(v,w)) | v in S and (v,w) in heavy(v)}" << endl;
+		cout << "Req = {(w; tent(v) + c(v,w)) | v in S and (v,w) in heavy(v)}" << endl;
 		list<pair<vertex_t, weight_t>> Req;
 		for (vertex_t v : S) {
 			for (edge_t e1 : E) {
-				for (edge_t e2 : heavy[v]) {
-					if (e1 == e2) {
+				for (int i=0; i<heavy[v].last; i++) {
+					if (e1 == heavy[v].edges[i]) {
 						weight_t weight = tent[v] + e1.weight;
-						Req.emplace_back(pair<vertex_t, weight_t>(e2.to, weight));
+						Req.emplace_back(pair<vertex_t, weight_t>(heavy[v].edges[i].to, weight));
 					}
 				}
 			}
 		}
 
-		// cout << "for each (v,x) in Req do relax(v,x)" << endl;
+		cout << "for each (v,x) in Req do relax(v,x)" << endl;
 		for (pair<vertex_t, weight_t> p : Req) {
 			relax(p.first, p.second, delta, tent, B);
 		}
 
 		B.erase(i);
 		i++;
-		// printTent(tent);
+		printTent(tent);
 	}
 
 	printTent(tent);
